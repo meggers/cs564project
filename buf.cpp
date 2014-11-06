b9eb8c230c5a8efd3debce79b32bd3245577d0ca
Matthew Wiemer <wiemer@galapagos-30.cs.wisc.edu>
2014-11-05 19:52:44 -0600

    Fixed bug in BufMgr::readPage() | testbuf now makes it to 'Testing Error Condition...'

--- buf.cpp
+++ buf.cpp
@@ -153,7 +153,7 @@ const Status BufMgr::readPage(File* file, const int PageNo, Page*& page) {
 			if(attempt != OK)
 				return attempt; // Return the error that was thrown in allocBuf()
 				
-			file->readPage(PageNo, page); // Read in new page; Not sure about parameters here
+			file->readPage(PageNo, &bufPool[frameNumber]); // Read in new page; Not sure about parameters here
 			hashTable->insert(file, PageNo, frameNumber); // Insert page into the hashTable
 			bufTable[frameNumber].Set(file, PageNo);
 			page = &(bufPool[frameNumber]); // Not sure if this is the right address...
